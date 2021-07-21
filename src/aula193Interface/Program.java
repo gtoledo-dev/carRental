626342fbec721293dfc4be7ae1d623b05a9690bb
gtoledo-dev <glauco_toledo@yahoo.com.br>
2021-07-21 16:18:25 -0300

    Projeto carRental parte 1

---
+++ src/aula193Interface/Program.java
@@ -0,0 +1,15 @@
+package aula193Interface;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+
+	}
+
+}
