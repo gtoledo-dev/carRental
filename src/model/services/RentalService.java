aeb79ebc66dc4e58c85fe07be1111e78b63b8bed
gtoledo-dev <glauco_toledo@yahoo.com.br>
2021-07-23 21:09:47 -0300

    correction in the math operation of the RentalService class

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -5,28 +5,30 @@ import model.entities.Invoice;
 
 public class RentalService {
 	
-	private Double pricePerHour;
 	private Double pricePerDay;
+	private Double pricePerHour;
 	
 	private TaxService taxService;
 
-	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
-		this.pricePerHour = pricePerHour;
+	
+	
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
 		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
 	}
-	
+
 	public void processInvoice(CarRental carRental) {
 		long t1 = carRental.getStart().getTime();
 		long t2 = carRental.getFinish().getTime();
-		double hours = (double)((t2 - t1) / 1000 / 60 / 60);
+		double hours = (double)(t2 - t1) / 1000 / 60 / 60;
 		
 		double basicPayment;
 		if(hours <= 12.0) {
 			basicPayment = Math.ceil(hours) * pricePerHour;
 			
 		} else {
-			basicPayment = Math.ceil(hours / 24 ) * pricePerDay;
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
 			
 		} double tax = taxService.tax(basicPayment);
 		
